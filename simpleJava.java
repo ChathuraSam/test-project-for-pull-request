8890a7b92d3b1cca0432e6b310844b86bb88219a
ChathuraSam <chathura.samarajeewa@gmail.com>
2018-10-25 21:48:37 +0530

    thid commit

--- simpleJava.java
+++ simpleJava.java
@@ -2,5 +2,6 @@ class Solution  {
   public static void main(String[] args) {
     System.out.println("Yea Im Working");
     System.out.println("Yea Im Working for second time");
+      System.out.println("Yea Im Working for third time");
   }
 }
